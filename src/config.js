e9411ac9013d2cb2f55a7febd83470656c243aa1
Heini Ahokas <ahokas.heini@gmail.com>
2019-12-15 17:47:10 +0100

    Set up AWS Amplify

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+	s3: {
+		REGION: 'us-east-1',
+		BUCKET: 'notes-app-files-upload'
+	},
+	apiGateway: {
+		REGION: 'us-east-1',
+		URL: 'https://5pfpuxi060.execute-api.us-east-1.amazonaws.com/prod'
+	},
+	cognito: {
+		REGION: 'us-east-1',
+		USER_POOL_ID: 'us-east-1_i6pLNGtdc',
+		APP_CLIENT_ID: '2pvvtgts03l745ruki2jfogfcm',
+		IDENTITY_POOL_ID: 'us-east-1:abfe086a-6b25-4983-b592-1750d4e01a87'
+	}
+};
