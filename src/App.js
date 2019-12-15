ef226896d4895f7c890d3a350a2957c15437ed18
Heini Ahokas <ahokas.heini@gmail.com>
2019-12-15 17:28:16 +0100

    Add react-router-bootstrap and wrap nav links for signup and lign with it

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Routes from "./Routes";
-import { Navbar } from 'react-bootstrap';
+import Routes from './Routes';
+import { Nav, Navbar, NavItem } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 import './App.css';
 
 export default function App(props) {
-  return (
-    <div className="App container">
-      <Navbar fluid collapseOnSelect>
-        <Navbar.Header>
-          <Navbar.Brand>
-            <Link to="/">Scratch</Link>
-          </Navbar.Brand>
-        </Navbar.Header>
-      </Navbar>
-      <Routes />
-    </div>
-  )
+	return (
+		<div className='App container'>
+			<Navbar fluid collapseOnSelect>
+				<Navbar.Header>
+					<Navbar.Brand>
+						<Link to='/'>Scratch</Link>
+					</Navbar.Brand>
+					<Navbar.Toggle />
+				</Navbar.Header>
+				<Navbar.Collapse>
+					<Nav pullRight>
+						<LinkContainer to='/signup'>
+							<NavItem>Signup</NavItem>
+						</LinkContainer>
+						<LinkContainer to='/login'>
+							<NavItem>Login</NavItem>
+						</LinkContainer>
+					</Nav>
+				</Navbar.Collapse>
+			</Navbar>
+			<Routes />
+		</div>
+	);
 }
-
