2973f86470cbc61726a6cf35f13609036c6a8506
Chrysalis-B <ahokas.heini@gmail.com>
2019-12-19 12:57:20 +0100

    Set up Notes list on homepage if user is authenticated

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Home.css';
+import { PageHeader, ListGroup } from 'react-bootstrap';
 
-export default function Home() {
-	return (
-		<div className='Home'>
+export default function Home(props) {
+	const [notes, setNotes] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+
+	function renderNotesList(notes) {
+		return null;
+	}
+
+	function renderLander() {
+		return (
 			<div className='lander'>
 				<h1>Scratch</h1>
 				<p>Make notes and stuff</p>
 			</div>
+		)
+	}
+
+	function renderNotes() {
+		return (
+			<div className='notes'>
+				<PageHeader>Your Notes</PageHeader>
+				<ListGroup>
+					{!isLoading && renderNotesList(notes)}
+				</ListGroup>
+
+			</div>)
+	}
+
+	return (
+		<div className='Home'>
+			{props.isAuthenticated ? renderNotes() : renderLander()}
 		</div>
-	);
+	)
+
 }
