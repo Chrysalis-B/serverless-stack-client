991dd17ac3f78d4031ab37f3600b5d58b89a1862
Heini Ahokas <ahokas.heini@gmail.com>
2019-12-29 22:53:39 +0200

    Add code-splitting for routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,51 +1,58 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import asyncComponent from './components/AsyncComponent';
 import AppliedRoute from './components/AppliedRoute';
-import AuthenticatedRoute from "./components/AuthenticatedRoute";
-import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
-import Home from './containers/Home';
-import Login from './containers/Login';
-import Signup from './containers/Signup';
-import Settings from './containers/Settings';
-import NewNote from './containers/NewNote';
-import Notes from './containers/Notes';
-import NotFound from './containers/NotFound';
+import AuthenticatedRoute from './components/AuthenticatedRoute';
+import UnauthenticatedRoute from './components/UnauthenticatedRoute';
+
+const AsyncHome = asyncComponent(() => import('./containers/Home'));
+const AsyncLogin = asyncComponent(() => import('./containers/Login'));
+const AsyncNotes = asyncComponent(() => import('./containers/Notes'));
+const AsyncSignup = asyncComponent(() => import('./containers/Signup'));
+const AsyncSettings = asyncComponent(() => import('./containers/Settings'));
+const AsyncNewNote = asyncComponent(() => import('./containers/NewNote'));
+const AsyncNotFound = asyncComponent(() => import('./containers/NotFound'));
 
 export default function Routes({ appProps }) {
 	return (
 		<Switch>
-			<AppliedRoute path='/' exact component={Home} appProps={appProps} />
+			<AppliedRoute
+				path='/'
+				exact
+				component={AsyncHome}
+				appProps={appProps}
+			/>
 			<UnauthenticatedRoute
 				path='/login'
 				exact
-				component={Login}
+				component={AsyncLogin}
 				appProps={appProps}
 			/>
 			<UnauthenticatedRoute
 				path='/signup'
 				exact
-				component={Signup}
+				component={AsyncSignup}
 				appProps={appProps}
 			/>
 			<AuthenticatedRoute
 				path='/settings'
 				exact
-				component={Settings}
+				component={AsyncSettings}
 				appProps={appProps}
 			/>
 			<AuthenticatedRoute
 				path='/notes/new'
 				exact
-				component={NewNote}
+				component={AsyncNewNote}
 				appProps={appProps}
 			/>
 			<AuthenticatedRoute
 				path='/notes/:id'
 				exact
-				component={Notes}
+				component={AsyncNotes}
 				appProps={appProps}
 			/>
-			<Route component={NotFound} />
+			<Route component={AsyncNotFound} />
 		</Switch>
 	);
 }
